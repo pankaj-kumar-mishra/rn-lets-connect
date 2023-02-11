9b1174ec843b91edfdb442fbfb4bfd4fd6eb5dc5
Pankaj Mishra <pankaj_mishra@epam.com>
2023-02-11 19:39:37 +0530

    navigation improved, undefined types improved

--- src/navigators/AppNavigator.tsx
+++ src/navigators/AppNavigator.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import HomeScreen from '../screens/HomeScreen'
+import MeetingScreen from '../screens/MeetingScreen'
 
 export type AppStackParamList = {
   Home: undefined
+  Meeting: {
+    meetingType:
+      | 'video_one_one'
+      | 'video_group'
+      | 'audio_one_one'
+      | 'audio_group'
+  }
   //   Profile: { userId: string } | undefined;
 }
 
@@ -15,6 +23,8 @@ const AppNavigator: FC<Props> = (): JSX.Element => {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={HomeScreen} />
+      {/* ZEGO CLOUD */}
+      <Stack.Screen name="Meeting" component={MeetingScreen} />
     </Stack.Navigator>
   )
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,63 @@
+import { NavigationProp } from '@react-navigation/native'
 import React, { FC } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Pressable, Alert } from 'react-native'
+import { AppStackParamList } from '../navigators/AppNavigator'
 
-interface Props {}
+interface Props {
+  navigation: NavigationProp<AppStackParamList, 'Home'>
+}
 
-const HomeScreen: FC<Props> = (): JSX.Element => {
+const HomeScreen: FC<Props> = ({ navigation }): JSX.Element => {
+  const handleVideoCallingOneToOne = () => {
+    navigation.navigate('Meeting', { meetingType: 'video_one_one' })
+  }
+  const handleVideoCallingGroup = () => {
+    Alert.alert('Sorry, Not yet implemented!')
+  }
+  const handleAudioCallingOneToOne = () => {
+    Alert.alert('Sorry, Not yet implemented!')
+  }
+  const handleAudioCallingGroup = () => {
+    Alert.alert('Sorry, Not yet implemented!')
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Pressable onPress={handleVideoCallingOneToOne} style={styles.button}>
+        <Text style={styles.text}>Video Calling (One to One)</Text>
+      </Pressable>
+      <Pressable onPress={handleVideoCallingGroup} style={styles.button}>
+        <Text style={styles.text}>Video Calling (Group)</Text>
+      </Pressable>
+      <Pressable onPress={handleAudioCallingOneToOne} style={styles.button}>
+        <Text style={styles.text}>Audio Calling (One to One)</Text>
+      </Pressable>
+      <Pressable onPress={handleAudioCallingGroup} style={styles.button}>
+        <Text style={styles.text}>Audio Calling (Group)</Text>
+      </Pressable>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  button: {
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '#00f',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 16,
+  },
+  text: {
+    fontWeight: '600',
+    color: '#00f',
+    fontSize: 16,
+  },
 })
 
 export default HomeScreen
